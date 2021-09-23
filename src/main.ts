d01ea128353f626f780f541494694630baf9aa53
omarawad0 <ogmal01@gmail.com>
2021-09-24 01:02:10 +0200

    initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import { createApp } from "vue";
+import "./assets/global.css";
+import Button from "ant-design-vue/lib/button";
+import Checkbox from "ant-design-vue/lib/checkbox";
+import List from "ant-design-vue/lib/list";
+import Input from "ant-design-vue/lib/input";
+import Spin from "ant-design-vue/lib/spin";
+import Form from "ant-design-vue/lib/form";
+import "ant-design-vue/dist/antd.css";
+import App from "./App.vue";
 
-createApp(App).use(store).mount('#app')
+import store from "./store";
+
+const app = createApp(App);
+app.use(List);
+app.use(Checkbox);
+app.use(Input);
+app.use(Button);
+app.use(Spin);
+app.use(Form);
+app.use(store);
+app.mount("#app");

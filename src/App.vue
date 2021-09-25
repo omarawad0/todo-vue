b86a8a4a1affd8b672f55c6f34b4a9063cd7249a
omarawad0 <ogmal01@gmail.com>
2021-09-25 20:59:34 +0200

    updated the App with the newly created components

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <h1>Hello</h1>
+  <div id="app">
+    <AddToDo />
+    <Todos />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AddToDo from "./components/AddToDo.vue";
+import Todos from "./components/Todos.vue";
 
 export default defineComponent({
-  name: "Todos",
-  components: {},
+  name: "App",
+  components: {
+    AddToDo,
+    Todos,
+  },
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  max-width: 1200px;
+  margin: 5rem auto 0 auto;
+  line-height: 1.4;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

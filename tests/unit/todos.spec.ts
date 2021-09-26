56f1003cf065660f78873a4aeeac8a5587df2799
omarawad0 <ogmal01@gmail.com>
2021-09-26 14:25:46 +0200

    finish it up

---
+++ tests/unit/todos.spec.ts
@@ -0,0 +1,12 @@
+/* import { mount } from "@vue/test-utils";
+import Todos from "@/components/Todos.vue";
+import { LIMIT } from "@/core/apis/todoApis";
+
+describe("Todos.vue", () => {
+  it("renders the fetched numbers of todos on first load", () => {
+    const wrapper = mount(Todos);
+    const todos = wrapper.findAll('data-test="todo"');
+    expect(todos).toHaveLength(LIMIT);
+  });
+});
+ */

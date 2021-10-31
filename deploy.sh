0bacb398c4ca323e3fab3e97ba548f6eb7b53e5a
omarawad0 <ogmal01@gmail.com>
2021-10-31 03:11:59 +0200

    vue deployed files

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+
+npm run build
+
+
+cd dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+git push -f git@github.com:omarawad0/todo-vue.git master:gh-pages
+
+cd -
